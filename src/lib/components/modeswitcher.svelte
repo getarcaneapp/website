682cee11426da9c3cabddbd86d0d75ba87091243
Kyle Mendell <ksm@ofkm.us>
2025-06-27 11:36:53 -0500

    fix: use ghost button for mode switcher

--- src/lib/components/modeswitcher.svelte
+++ src/lib/components/modeswitcher.svelte
@@ -5,7 +5,7 @@
 	import { Button } from '$lib/components/ui/button/index.js'; // Adjust path if needed
 </script>
 
-<Button onclick={toggleMode} variant="outline" size="icon">
+<Button onclick={toggleMode} variant="ghost" size="icon">
 	<SunIcon
 		class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 !transition-all dark:scale-0 dark:-rotate-90"
 	/>
