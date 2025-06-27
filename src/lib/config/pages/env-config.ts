7f95a7c9f2bb53f808108d2f261dd35dbe0cbcd8
Kyle Mendell <ksm@ofkm.us>
2025-06-27 14:09:57 -0500

    chore: use static definetiosn for env table

---
+++ src/lib/config/pages/env-config.ts
@@ -0,0 +1,83 @@
+export type EnvConfig = {
+	name: string;
+	description: string;
+	value: string;
+};
+
+export const envConfig: EnvConfig[] = [
+	{
+		name: 'PORT',
+		description: 'The port arcane should run on',
+		value: '8080'
+	},
+	{
+		name: 'DEV_BACKEND_URL',
+		description: 'The url of the backend for development',
+		value: 'http://localhost:8080'
+	},
+	{
+		name: 'PUID',
+		description: 'File owner user ID',
+		value: '2000'
+	},
+	{
+		name: 'PGID',
+		description: 'File owner group ID',
+		value: '2000'
+	},
+	{
+		name: 'DOCKER_GID',
+		description: 'Docker group ID',
+		value: '(auto)'
+	},
+	{
+		name: 'ENCRYPTION_KEY',
+		description: 'Encryption Key for secure stored sensitive data',
+		value: '-'
+	},
+	{
+		name: 'JWT_SECRET',
+		description: 'Session secret',
+		value: '-'
+	},
+	{
+		name: 'OIDC_ENABLED',
+		description: 'Enable OIDC login',
+		value: 'false'
+	},
+	{
+		name: 'OIDC_CLIENT_ID',
+		description: 'Client ID from your OIDC provider',
+		value: 'your_arcane_client_id_from_provider'
+	},
+	{
+		name: 'OIDC_CLIENT_SECRET',
+		description: 'Client Secret from provider',
+		value: 'your_client_secret_from_provider'
+	},
+	{
+		name: 'OIDC_REDIRECT_URI',
+		description: 'Redirect URI (must match provider)',
+		value: 'http://localhost:3000/auth/oidc/callback'
+	},
+	{
+		name: 'OIDC_AUTHORIZATION_ENDPOINT',
+		description: 'Auth endpoint URL',
+		value: 'https://your-provider.com/oidc/authorize'
+	},
+	{
+		name: 'OIDC_TOKEN_ENDPOINT',
+		description: 'Token endpoint URL',
+		value: 'https://your-provider.com/oidc/token'
+	},
+	{
+		name: 'OIDC_USERINFO_ENDPOINT',
+		description: 'Userinfo endpoint URL',
+		value: 'https://your-provider.com/oidc/userinfo'
+	},
+	{
+		name: 'OIDC_SCOPES',
+		description: 'Scopes to request',
+		value: 'openid email profile (default)'
+	}
+];
