c119860f4782d025c66cffed323cd7eb6ead4b08
Kyle Mendell <ksm@ofkm.us>
2026-02-11 13:23:13 -0600

    format

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,28 @@
-import { enhancedImages } from "@sveltejs/enhanced-img";
-import { sveltekit } from "@sveltejs/kit/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { enhancedImages } from '@sveltejs/enhanced-img';
+import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  optimizeDeps: {
-    exclude: ["@lucide/svelte"],
-  },
-  plugins: [tailwindcss(), enhancedImages(), sveltekit()],
-  server: {
-    fs: {
-      allow: ["..", "./content"],
-    },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("@lucide/svelte")) {
-            return "icons";
-          }
-        },
-      },
-    },
-    minify: "oxc",
-  },
+	optimizeDeps: {
+		exclude: ['@lucide/svelte']
+	},
+	plugins: [tailwindcss(), enhancedImages(), sveltekit()],
+	server: {
+		fs: {
+			allow: ['..', './content']
+		}
+	},
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes('@lucide/svelte')) {
+						return 'icons';
+					}
+				}
+			}
+		},
+		minify: 'oxc'
+	}
 });
